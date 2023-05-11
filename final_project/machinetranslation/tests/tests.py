6f5621b1a9a06bdfa64ee682579ac71ecdf87f61
rcowdry <55250849+rcowdry@users.noreply.github.com>
2023-05-11 14:45:58 -0400

    Final Commit

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,5 +1,11 @@
 import unittest
+import os
+import sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 
+# import translator
 from translator import english_to_french, french_to_english
 
 
